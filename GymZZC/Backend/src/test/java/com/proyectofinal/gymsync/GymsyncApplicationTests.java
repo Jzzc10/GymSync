7305b8fb8e6825e581318acb71a5a06206e3139e
ZZC\jorge <jorgezhou10@gmail.com>
2025-06-02 07:08:39 +0200

    Add initial Spring Boot application files

---
+++ GymZZC/Backend/src/test/java/com/proyectofinal/gymsync/GymsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.gymsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
